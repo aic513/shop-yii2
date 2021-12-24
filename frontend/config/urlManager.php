103e037d5e2be1c005c4d4e382e00c90d4617d8d
aic513 <aic513@mail.ru>
2021-12-24 23:55:01 +0300

    add tag

--- frontend/config/urlManager.php
+++ frontend/config/urlManager.php
@@ -3,20 +3,20 @@
 /** @var array $params */
 
 return [
-    'class' => 'yii\web\UrlManager',
-    'hostInfo' => $params['frontendHostInfo'],
-    'enablePrettyUrl' => true,
-    'showScriptName' => true,
-    'rules' => [
-        '' => 'site/index',
-        '<_a:about>' => 'site/<_a>',
-        'contact' => 'contact/index',
-        'signup' => 'auth/signup/request',
-        'signup/<_a:[\w-]+>' => 'auth/signup/<_a>',
-        '<_a:login|logout>' => 'auth/auth/<_a>',
+	'class' => 'yii\web\UrlManager',
+	'hostInfo' => $params['frontendHostInfo'],
+	'enablePrettyUrl' => true,
+	'showScriptName' => false,
+	'rules' => [
+		'' => 'site/index',
+		'<_a:about>' => 'site/<_a>',
+		'contact' => 'contact/index',
+		'signup' => 'auth/signup/request',
+		'signup/<_a:[\w-]+>' => 'auth/signup/<_a>',
+		'<_a:login|logout>' => 'auth/auth/<_a>',
 
-        'cabinet' => 'cabinet/default/index',
-        'cabinet/<_c:[\w\-]+>' => 'cabinet/<_c>/index',
+		'cabinet' => 'cabinet/default/index',
+		'cabinet/<_c:[\w\-]+>' => 'cabinet/<_c>/index',
         'cabinet/<_c:[\w\-]+>/<id:\d+>' => 'cabinet/<_c>/view',
         'cabinet/<_c:[\w\-]+>/<_a:[\w-]+>' => 'cabinet/<_c>/<_a>',
         'cabinet/<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => 'cabinet/<_c>/<_a>',
