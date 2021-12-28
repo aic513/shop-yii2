fd2c8dd2e2f56fb4abbe89c48e1b2219e92f8da8
denis <aic513@mail.ru>
2021-12-28 22:39:51 +0300

    add categories management

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -25,24 +25,25 @@
             </div>
         </form>
         <!-- /.search form -->
-
-	    <?= dmstr\widgets\Menu::widget(
-		    [
-			    'options' => ['class' => 'sidebar-menu tree', 'data-widget' => 'tree'],
-			    'items' => [
-				    ['label' => 'Management', 'options' => ['class' => 'header']],
-				    [
-					    'label' => 'Shop',
-					    'icon' => 'folder',
-					    'items' => [
-						    ['label' => 'Brands', 'icon' => 'file-o', 'url' => ['/shop/brand/index'], 'active' => $this->context->id == 'shop/brand'],
-						    ['label' => 'Tags', 'icon' => 'file-o', 'url' => ['/shop/tag/index'], 'active' => $this->context->id == 'shop/tag'],
-					    ]
-				    ],
-				    ['label' => 'Users', 'icon' => 'user', 'url' => ['/user/index'], 'active' => $this->context->id == 'user/index'],
-			    ],
-		    ]
-	    ) ?>
+    
+        <?= dmstr\widgets\Menu::widget(
+            [
+                'options' => ['class' => 'sidebar-menu tree', 'data-widget' => 'tree'],
+                'items' => [
+                    ['label' => 'Management', 'options' => ['class' => 'header']],
+                    [
+                        'label' => 'Shop',
+                        'icon' => 'folder',
+                        'items' => [
+                            ['label' => 'Brands', 'icon' => 'file-o', 'url' => ['/shop/brand/index'], 'active' => $this->context->id == 'shop/brand'],
+                            ['label' => 'Tags', 'icon' => 'file-o', 'url' => ['/shop/tag/index'], 'active' => $this->context->id == 'shop/tag'],
+                            ['label' => 'Categories', 'icon' => 'file-o', 'url' => ['/shop/category/index'], 'active' => $this->context->id == 'shop/category'],
+                        ]
+                    ],
+                    ['label' => 'Users', 'icon' => 'user', 'url' => ['/user/index'], 'active' => $this->context->id == 'user/index'],
+                ],
+            ]
+        ) ?>
 
     </section>
 
