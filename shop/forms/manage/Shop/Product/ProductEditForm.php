233ccb7a19de975bf22aa64880b153293fe6bf17
aic513 <aic513@mail.ru>
2021-12-29 20:15:55 +0300

    move categories into product edit form

--- shop/forms/manage/Shop/Product/ProductEditForm.php
+++ shop/forms/manage/Shop/Product/ProductEditForm.php
@@ -8,47 +8,48 @@ use shop\forms\CompositeForm;
 use shop\forms\manage\MetaForm;
 
 /**
- * @property MetaForm $meta
+ * @property MetaForm       $meta
  * @property CategoriesForm $categories
- * @property TagsForm $tags
- * @property ValueForm[] $values
+ * @property TagsForm       $tags
+ * @property ValueForm[]    $values
  */
 class ProductEditForm extends CompositeForm
 {
-	public $brandId;
-
-	public $code;
-
-	public $name;
-
-	private $_product;
-
-	public function __construct(Product $product, $config = [])
-	{
-		$this->brandId = $product->brand_id;
-		$this->code = $product->code;
-		$this->name = $product->name;
-		$this->meta = new MetaForm($product->meta);
-		$this->tags = new TagsForm($product);
-		$this->values = array_map(function (Characteristic $characteristic) use ($product) {
-			return new ValueForm($characteristic, $product->getValue($characteristic->id));
-		}, Characteristic::find()->orderBy('sort')->all());
-		$this->_product = $product;
-		parent::__construct($config);
-	}
-
-	public function rules(): array
-	{
-		return [
-			[['brandId', 'code', 'name'], 'required'],
-			[['brandId'], 'integer'],
-			[['code', 'name'], 'string', 'max' => 255],
-			[['code'], 'unique', 'targetClass' => Product::class, 'filter' => $this->_product ? ['<>', 'id', $this->_product->id] : null],
-		];
-	}
-
-	protected function internalForms(): array
-	{
-		return ['meta', 'tags', 'values'];
-	}
+    public $brandId;
+    
+    public $code;
+    
+    public $name;
+    
+    private $_product;
+    
+    public function __construct(Product $product, $config = [])
+    {
+        $this->brandId = $product->brand_id;
+        $this->code = $product->code;
+        $this->name = $product->name;
+        $this->meta = new MetaForm($product->meta);
+        $this->categories = new CategoriesForm($product);
+        $this->tags = new TagsForm($product);
+        $this->values = array_map(function (Characteristic $characteristic) use ($product) {
+            return new ValueForm($characteristic, $product->getValue($characteristic->id));
+        }, Characteristic::find()->orderBy('sort')->all());
+        $this->_product = $product;
+        parent::__construct($config);
+    }
+    
+    public function rules(): array
+    {
+        return [
+            [['brandId', 'code', 'name'], 'required'],
+            [['brandId'], 'integer'],
+            [['code', 'name'], 'string', 'max' => 255],
+            [['code'], 'unique', 'targetClass' => Product::class, 'filter' => $this->_product ? ['<>', 'id', $this->_product->id] : null],
+        ];
+    }
+    
+    protected function internalForms(): array
+    {
+        return ['meta', 'tags', 'values'];
+    }
 }
