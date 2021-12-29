2c35d02c0395faf6324d708adfa73661f1b2e683
aic513 <aic513@mail.ru>
2021-12-29 17:50:30 +0300

    add category removing restriction

--- shop/repositories/Shop/ProductRepository.php
+++ shop/repositories/Shop/ProductRepository.php
@@ -7,31 +7,36 @@ use shop\repositories\NotFoundException;
 
 class ProductRepository
 {
-	public function get($id): Product
-	{
-		if (!$product = Product::findOne($id)) {
-			throw new NotFoundException('Product is not found.');
-		}
-
-		return $product;
-	}
-
-	public function existsByBrand($id): bool
-	{
-		return Product::find()->andWhere(['brand_id' => $id])->exists();
-	}
-
-	public function save(Product $product): void
-	{
-		if (!$product->save()) {
-			throw new \RuntimeException('Saving error.');
-		}
-	}
-
-	public function remove(Product $product): void
-	{
-		if (!$product->delete()) {
-			throw new \RuntimeException('Removing error.');
-		}
-	}
+    public function get($id): Product
+    {
+        if (!$product = Product::findOne($id)) {
+            throw new NotFoundException('Product is not found.');
+        }
+        
+        return $product;
+    }
+    
+    public function existsByBrand($id): bool
+    {
+        return Product::find()->andWhere(['brand_id' => $id])->exists();
+    }
+    
+    public function existsByMainCategory($id): bool
+    {
+        return Product::find()->andWhere(['category_id' => $id])->exists();
+    }
+    
+    public function save(Product $product): void
+    {
+        if (!$product->save()) {
+            throw new \RuntimeException('Saving error.');
+        }
+    }
+    
+    public function remove(Product $product): void
+    {
+        if (!$product->delete()) {
+            throw new \RuntimeException('Removing error.');
+        }
+    }
 }
--- shop/services/manage/Shop/CategoryManageService.php
+++ shop/services/manage/Shop/CategoryManageService.php
@@ -6,69 +6,76 @@ use shop\entities\Meta;
 use shop\entities\Shop\Category;
 use shop\forms\manage\Shop\CategoryForm;
 use shop\repositories\Shop\CategoryRepository;
+use shop\repositories\Shop\ProductRepository;
 
 class CategoryManageService
 {
-	private $categories;
-
-	public function __construct(CategoryRepository $categories)
-	{
-		$this->categories = $categories;
-	}
-
-	public function create(CategoryForm $form): Category
-	{
-		$parent = $this->categories->get($form->parentId);
-		$category = Category::create(
-			$form->name,
-			$form->slug,
-			$form->title,
-			$form->description,
-			new Meta(
-				$form->meta->title,
-				$form->meta->description,
-				$form->meta->keywords
-			)
-		);
-		$category->appendTo($parent);
-		$this->categories->save($category);
-
-		return $category;
-	}
-
-	public function edit($id, CategoryForm $form): void
-	{
-		$category = $this->categories->get($id);
-		$this->assertIsNotRoot($category);
-		$category->edit(
-			$form->name,
-			$form->slug,
-			$form->title,
-			$form->description,
-			new Meta(
-				$form->meta->title,
-				$form->meta->description,
-				$form->meta->keywords
-			)
-		);
-		if ($form->parentId !== $category->parent->id) {
-			$parent = $this->categories->get($form->parentId);
-			$category->appendTo($parent);
-		}
-		$this->categories->save($category);
-	}
-
-	public function remove($id): void
-	{
-		$category = $this->categories->get($id);
-		$this->assertIsNotRoot($category);
-		$this->categories->remove($category);
-	}
-
-	private function assertIsNotRoot(Category $category): void
-	{
-		if ($category->isRoot()) {
-			throw new \DomainException('Unable to manage the root category.');
-		}
-	}
+    private $categories;
+    
+    private $products;
+    
+    public function __construct(CategoryRepository $categories, ProductRepository $products)
+    {
+        $this->categories = $categories;
+        $this->products = $products;
+    }
+    
+    public function create(CategoryForm $form): Category
+    {
+        $parent = $this->categories->get($form->parentId);
+        $category = Category::create(
+            $form->name,
+            $form->slug,
+            $form->title,
+            $form->description,
+            new Meta(
+                $form->meta->title,
+                $form->meta->description,
+                $form->meta->keywords
+            )
+        );
+        $category->appendTo($parent);
+        $this->categories->save($category);
+        
+        return $category;
+    }
+    
+    public function edit($id, CategoryForm $form): void
+    {
+        $category = $this->categories->get($id);
+        $this->assertIsNotRoot($category);
+        $category->edit(
+            $form->name,
+            $form->slug,
+            $form->title,
+            $form->description,
+            new Meta(
+                $form->meta->title,
+                $form->meta->description,
+                $form->meta->keywords
+            )
+        );
+        if ($form->parentId !== $category->parent->id) {
+            $parent = $this->categories->get($form->parentId);
+            $category->appendTo($parent);
+        }
+        $this->categories->save($category);
+    }
+    
+    public function remove($id): void
+    {
+        $category = $this->categories->get($id);
+        $this->assertIsNotRoot($category);
+        if ($this->products->existsByMainCategory($category->id)) {
+            throw new \DomainException('Unable to remove category with products.');
+        }
+        $this->categories->remove($category);
+    }
+    
+    private function assertIsNotRoot(Category $category): void
+    {
+        if ($category->isRoot()) {
+            throw new \DomainException('Unable to manage the root category.');
+        }
+    }
 }
