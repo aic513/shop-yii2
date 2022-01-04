4050295b36bba6d02fc1121b830dfaff6a1abc2e
aic513 <aic513@mail.ru>
2022-01-04 21:15:00 +0300

    add category url caching

--- common/bootstrap/SetUp.php
+++ common/bootstrap/SetUp.php
@@ -3,19 +3,25 @@
 namespace common\bootstrap;
 
 use shop\services\contact\ContactService;
+use Yii;
 use yii\base\BootstrapInterface;
+use yii\caching\Cache;
 use yii\mail\MailerInterface;
 
 class SetUp implements BootstrapInterface
 {
     public function bootstrap($app): void
     {
-        $container = \Yii::$container;
-
+        $container = Yii::$container;
+    
         $container->setSingleton(MailerInterface::class, function () use ($app) {
             return $app->mailer;
         });
-
+    
+        $container->setSingleton(Cache::class, function () use ($app) {
+            return $app->cache;
+        });
+    
         $container->setSingleton(ContactService::class, [], [
             $app->params['adminEmail']
         ]);
