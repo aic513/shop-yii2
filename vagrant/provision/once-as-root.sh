44c39947149ce8e60ae6c1b45da5368ab98829b1
aic513 <aic513@mail.ru>
2022-01-04 23:29:30 +0300

    reinstall elasticsearch

--- vagrant/provision/once-as-root.sh
+++ vagrant/provision/once-as-root.sh
@@ -9,7 +9,7 @@ readonly IP=$2
 
 #== Provision script ==
 
-info "Provision-script user: `whoami`"
+info "Provision-script user: $(whoami)"
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -20,12 +20,18 @@ info "AWK initial replacement work"
 awk -v ip=$IP -f /app/vagrant/provision/provision.awk /app/environments/dev/*end/config/main-local.php
 
 info "Prepare root password for MySQL"
-debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password \"''\""
-debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password \"''\""
+debconf-set-selections <<<"mysql-community-server mysql-community-server/root-pass password \"''\""
+debconf-set-selections <<<"mysql-community-server mysql-community-server/re-root-pass password \"''\""
 echo "Done!"
+
+info "Add ppa:ondrej/php"
+apt-get install -y python-software-properties
+apt-get update && apt-get upgrade -y
+add-apt-repository -y ppa:ondrej/php
 
 info "Add Oracle JDK repository"
 add-apt-repository ppa:webupd8team/java -y
+
 info "Add ElasticSearch sources"
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-5.x.list
@@ -34,17 +40,12 @@ info "Update OS software"
 apt-get update
 apt-get upgrade -y
 
-info "Add ppa:ondrej/php"
-apt-get install -y python-software-properties
-apt-get update && apt-get upgrade -y
-add-apt-repository -y ppa:ondrej/php
-
 info "Install additional software"
 apt-get install -y php7.4-curl php7.4-cli php7.4-intl php7.4-mysqlnd php7.4-gd php7.4-fpm php7.4-mbstring php7.4-xml unzip nginx mysql-server-5.7 php7.4-xdebug
 
 info "Install Oracle JDK"
-debconf-set-selections <<< "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true"
-debconf-set-selections <<< "oracle-java8-installer shared/accepted-oracle-license-v1-1 seen true"
+debconf-set-selections <<<"oracle-java8-installer shared/accepted-oracle-license-v1-1 select true"
+debconf-set-selections <<<"oracle-java8-installer shared/accepted-oracle-license-v1-1 seen true"
 apt-get install -y oracle-java8-installer
 
 info "Install ElasticSearch"
@@ -55,17 +56,17 @@ service elasticsearch restart
 
 info "Configure MySQL"
 sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
-mysql -uroot <<< "CREATE USER 'root'@'%' IDENTIFIED BY ''"
-mysql -uroot <<< "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'"
-mysql -uroot <<< "DROP USER 'root'@'localhost'"
-mysql -uroot <<< "FLUSH PRIVILEGES"
+mysql -uroot <<<"CREATE USER 'root'@'%' IDENTIFIED BY ''"
+mysql -uroot <<<"GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'"
+mysql -uroot <<<"DROP USER 'root'@'localhost'"
+mysql -uroot <<<"FLUSH PRIVILEGES"
 echo "Done!"
 
 info "Configure PHP-FPM"
 sed -i 's/user = www-data/user = vagrant/g' /etc/php/7.4/fpm/pool.d/www.conf
 sed -i 's/group = www-data/group = vagrant/g' /etc/php/7.4/fpm/pool.d/www.conf
 sed -i 's/owner = www-data/owner = vagrant/g' /etc/php/7.4/fpm/pool.d/www.conf
-cat << EOF > /etc/php/7.4/mods-available/xdebug.ini
+cat <<EOF >/etc/php/7.4/mods-available/xdebug.ini
 zend_extension=xdebug.so
 xdebug.remote_enable=1
 xdebug.remote_connect_back=1
@@ -83,8 +84,8 @@ ln -s /app/vagrant/nginx/app.conf /etc/nginx/sites-enabled/app.conf
 echo "Done!"
 
 info "Initailize databases for MySQL"
-mysql -uroot <<< "CREATE DATABASE shop"
-mysql -uroot <<< "CREATE DATABASE shop_test"
+mysql -uroot <<<"CREATE DATABASE shop"
+mysql -uroot <<<"CREATE DATABASE shop_test"
 echo "Done!"
 
 info "Install composer"
