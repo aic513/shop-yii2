07aceb556e1703deca3385cfd807292fc18d965c
aic513 <aic513@mail.ru>
2022-01-04 23:28:29 +0300

    install ElasticSearch library

--- common/bootstrap/SetUp.php
+++ common/bootstrap/SetUp.php
@@ -2,6 +2,8 @@
 
 namespace common\bootstrap;
 
+use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder;
 use shop\services\contact\ContactService;
 use Yii;
 use yii\base\BootstrapInterface;
@@ -13,15 +15,19 @@ class SetUp implements BootstrapInterface
     public function bootstrap($app): void
     {
         $container = Yii::$container;
-    
+        
         $container->setSingleton(MailerInterface::class, function () use ($app) {
             return $app->mailer;
         });
-    
+        
         $container->setSingleton(Cache::class, function () use ($app) {
             return $app->cache;
         });
-    
+        
+        $container->setSingleton(Client::class, function () {
+            return ClientBuilder::create()->build();
+        });
+        
         $container->setSingleton(ContactService::class, [], [
             $app->params['adminEmail']
         ]);
