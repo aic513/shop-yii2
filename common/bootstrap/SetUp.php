51d8a284554d7f45ee0c68d49455e009919108ae
aic513 <aic513@mail.ru>
2022-01-27 23:57:58 +0300

    add Yandex Market XML generator

--- common/bootstrap/SetUp.php
+++ common/bootstrap/SetUp.php
@@ -9,6 +9,8 @@ use shop\cart\cost\calculator\DynamicCost;
 use shop\cart\cost\calculator\SimpleCost;
 use shop\cart\storage\HybridStorage;
 use shop\services\contact\ContactService;
+use shop\services\yandex\ShopInfo;
+use shop\services\yandex\YandexMarket;
 use Yii;
 use yii\base\BootstrapInterface;
 use yii\caching\Cache;
@@ -24,28 +26,32 @@ class SetUp implements BootstrapInterface
         $container->setSingleton(MailerInterface::class, function () use ($app) {
             return $app->mailer;
         });
-    
+        
         $container->setSingleton(Cache::class, function () use ($app) {
             return $app->cache;
         });
-    
+        
         $container->setSingleton(Client::class, function () {
             return ClientBuilder::create()->build();
         });
-    
+        
         $container->setSingleton(ManagerInterface::class, function () use ($app) {
             return $app->authManager;
         });
-    
+        
         $container->setSingleton(ContactService::class, [], [
             $app->params['adminEmail']
         ]);
-    
+        
         $container->setSingleton(Cart::class, function () use ($app) {
             return new Cart(
                 new HybridStorage($app->get('user'), 'cart', 3600 * 24, $app->db),
                 new DynamicCost(new SimpleCost())
             );
         });
+        
+        $container->setSingleton(YandexMarket::class, [], [
+            new ShopInfo($app->name, $app->name, $app->params['frontendHostInfo']),
+        ]);
     }
 }
