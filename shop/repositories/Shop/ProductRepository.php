8c99c1316fe0a4b564a27b8fcd614f73fc81a988
aic513 <aic513@mail.ru>
2022-02-02 16:44:14 +0300

    add persistance events

--- shop/repositories/Shop/ProductRepository.php
+++ shop/repositories/Shop/ProductRepository.php
@@ -5,6 +5,8 @@ namespace shop\repositories\Shop;
 use RuntimeException;
 use shop\dispatchers\EventDispatcher;
 use shop\entities\Shop\Product\Product;
+use shop\repositories\events\EntityPersisted;
+use shop\repositories\events\EntityRemoved;
 use shop\repositories\NotFoundException;
 
 class ProductRepository
@@ -41,6 +43,7 @@ class ProductRepository
             throw new RuntimeException('Saving error.');
         }
         $this->dispatcher->dispatchAll($product->releaseEvents());
+        $this->dispatcher->dispatch(new EntityPersisted($product));
     }
     
     public function remove(Product $product): void
@@ -49,5 +52,6 @@ class ProductRepository
             throw new RuntimeException('Removing error.');
         }
         $this->dispatcher->dispatchAll($product->releaseEvents());
+        $this->dispatcher->dispatch(new EntityRemoved($product));
     }
 }
